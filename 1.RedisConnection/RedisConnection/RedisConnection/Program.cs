1f9728b97ee36d84365ce8c2d5224532c5072dd8
taswar <taswar@zeytinsoft.com>
2016-07-03 18:43:45 -0400

    Redis Connection Sample Program

---
+++ 1.RedisConnection/RedisConnection/RedisConnection/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RedisConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var redis = RedisStore.RedisCache;
+
+            if (redis.StringSet("testKey", "testValue"))
+            {
+                var val = redis.StringGet("testKey");
+
+                Console.WriteLine(val);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1.RedisConnection/RedisConnection/RedisConnection/RedisStore.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Configuration;
+using StackExchange.Redis;
+
+namespace RedisConnection
+{
+    public class RedisStore
+    {
+        private static readonly Lazy<ConnectionMultiplexer> LazyConnection;
+
+        static RedisStore()
+        {
+            var configurationOptions = new ConfigurationOptions
+            {
+                EndPoints = { ConfigurationManager.AppSettings["redis.connection"] }
+            };
+
+            LazyConnection = new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(configurationOptions));
+        }
+
+        public static ConnectionMultiplexer Connection => LazyConnection.Value;
+
+        public static IDatabase RedisCache => Connection.GetDatabase();
+    }
+}
